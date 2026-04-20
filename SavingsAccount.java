9b978ecc9afd73cad71ce5bddd8bc4895e83b2c5
mikaylajd7-ship-it <mikaylajd7@gmail.com>
2026-04-19 22:18:51 -0400

    Update SavingsAccount.java

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,3 +1,10 @@
+/**
+ * Name: Mikayla Dickerson
+ * Date: 04/19/2026
+ * Assignment: SDC330 Course Project – Phase 2
+ * Description: Represents a savings account with interest functionality.
+ * Inherits from Account and demonstrates inheritance and polymorphism.
+ */
 public class SavingsAccount extends Account {
 
     public SavingsAccount(String owner, double balance) {
