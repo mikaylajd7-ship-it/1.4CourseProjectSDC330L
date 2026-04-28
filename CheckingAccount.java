53b04522061ca007915a8b36bf2d63e935112fc3
mikaylajd7-ship-it <mikaylajd7@gmail.com>
2026-04-28 14:41:31 -0400

    Update CheckingAccount.java

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,16 +1,32 @@
 /*******************************************************************
  * Name: Mikayla Dickerson
- * Date: 04/12/2026
- * Purpose: Demonstrates inheritance (Checking Account)
+ * Date: 04/28/2026
+ * Purpose: Represents a checking account.
+ * Inherits from Account and overrides abstract methods.
  *******************************************************************/
+
 public class CheckingAccount extends Account {
 
+    // Default constructor
+    public CheckingAccount() {
+        super();
+    }
+
+    // Parameterized constructor
     public CheckingAccount(String owner, double balance) {
         super(owner, balance);
     }
 
+    // Monthly service fee
     @Override
-    public String getAccountType() {
-        return "Checking";
+    public void monthlyUpdate() {
+        updateBalance(-10.00);
+    }
+
+    // Display checking account info
+    @Override
+    public void displayAccountInfo() {
+        System.out.println("Checking Account");
+        super.displayAccountInfo();
     }
 }
