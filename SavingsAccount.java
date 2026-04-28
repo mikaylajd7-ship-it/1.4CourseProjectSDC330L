34b2950a8407e54b7bf56108ccefda65b3411298
mikaylajd7-ship-it <mikaylajd7@gmail.com>
2026-04-28 14:41:56 -0400

    Update SavingsAccount.java

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,16 +1,32 @@
 /*******************************************************************
  * Name: Mikayla Dickerson
- * Date: 04/19/2026
- * Purpose: Demonstrates inheritance (Savings Account)
+ * Date: 04/28/2026
+ * Purpose: Represents a savings account.
+ * Inherits from Account and overrides abstract methods.
  *******************************************************************/
+
 public class SavingsAccount extends Account {
 
+    // Default constructor
+    public SavingsAccount() {
+        super();
+    }
+
+    // Parameterized constructor
     public SavingsAccount(String owner, double balance) {
         super(owner, balance);
     }
 
+    // Monthly interest added
     @Override
-    public String getAccountType() {
-        return "Savings";
+    public void monthlyUpdate() {
+        updateBalance(25.00);
+    }
+
+    // Display savings account info
+    @Override
+    public void displayAccountInfo() {
+        System.out.println("Savings Account");
+        super.displayAccountInfo();
     }
 }
